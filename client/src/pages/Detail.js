a908d00c51e0e6573e169a1f5bbc79d442de3671
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-10-27 21:23:04 -0400

    created Cart and CartItem components

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
 
+import Cart from '../components/Cart';
+
 import { QUERY_PRODUCTS } from "../utils/queries";
 import { UPDATE_PRODUCTS } from '../utils/actions';
 import { useStoreContext } from '../utils/GlobalState';
@@ -64,6 +66,7 @@ function Detail() {
       {
         loading ? <img src={spinner} alt="loading" /> : null
       }
+      <Cart />
     </>
   );
 };
