c2392ac3d9e9e5129e5fc11db459ad091ce454fd
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-10-28 01:49:29 -0400

    can now add, remove, or update quantity of items in cart

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,6 +1,34 @@
 import React from 'react';
+import { useStoreContext } from '../../utils/GlobalState';
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
 const CartItem = ({ item }) => {
+  const [, dispatch] = useStoreContext();
+
+  const removeFromCart = () => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: item._id
+    });
+  };
+
+  const handleChange = event => {
+    const value = event.target.value;
+
+    if (value === '0' || !value) {
+      dispatch({
+        type: REMOVE_FROM_CART,
+        _id: item._id
+      });
+    } else {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: item._id,
+        purchaseQuantity: parseInt(value)
+      });
+    }
+  };
+
   return (
     <div className="flex-row">
       <div>
@@ -15,12 +43,13 @@ const CartItem = ({ item }) => {
           <span>Qty:</span>
           <input
             type="number"
-            placeholder="1"
             value={item.purchaseQuantity}
+            onChange={handleChange}
           />
           <span
             role="img"
             aria-label="trash"
+            onClick={removeFromCart}
           >
             🗑️
           </span>
