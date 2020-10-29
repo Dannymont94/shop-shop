8a5fcbe2ac4fb7258ecee222a2c9825c508b099b
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-10-29 15:10:09 -0400

    removing and updating purchaseQuantity from the cart itself now updates IndexedDB

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,29 +1,45 @@
 import React from 'react';
 import { useStoreContext } from '../../utils/GlobalState';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise } from '../../utils/helpers';
 
 const CartItem = ({ item }) => {
   const [, dispatch] = useStoreContext();
 
   const removeFromCart = () => {
+    // update global state
     dispatch({
       type: REMOVE_FROM_CART,
       _id: item._id
     });
+
+    // update IndexedDB
+    idbPromise('cart', 'delete', { ...item });
   };
 
   const handleChange = event => {
     const value = event.target.value;
 
     if (value === '0' || !value) {
+    // update global state
       dispatch({
         type: REMOVE_FROM_CART,
         _id: item._id
       });
+
+      // update IndexedDB
+      idbPromise('cart', 'delete', { ...item });
     } else {
+      // update global state
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: item._id,
+        purchaseQuantity: parseInt(value)
+      });
+
+      // update IndexedDB
+      idbPromise('cart', 'put', {
+        ...item,
         purchaseQuantity: parseInt(value)
       });
     }
